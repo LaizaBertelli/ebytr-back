d1cd535e12511ff3f3bc85cf2c61915317a908bb
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-13 04:12:34 -0300

    fix: fix dotenv reading path

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: 'task_manager',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
   },
   test: {
