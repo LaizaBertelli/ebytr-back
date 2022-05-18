0920405a8c522a054e4c8233f06f3b2cdfb79d7c
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-18 16:53:21 -0300

    feat: encode password at login service

--- src/Services/login.service.js
+++ src/Services/login.service.js
@@ -1,18 +1,21 @@
 const { generateToken } = require('../Auth/generateToken');
 const { Users } = require('../database/models');
+const encodePassword = require('../Helpers/encodePassword');
 
 const postLogin = async (email, password) => {
   try {
+    const encodePass = encodePassword(password);
     const user = await Users.findOne({
       raw: true,
       where: {
         email,
-        password,
+        password: encodePass,
       },
       attributes: { exclude: ['password']}
     });
 
     if (!user) return null;
+
     const token = generateToken(user);
     return token;
   } catch (e) {
