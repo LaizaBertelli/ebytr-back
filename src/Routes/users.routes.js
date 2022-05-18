59648d8ab5fd9f6d344264d5a6cf1c9e5b4eab20
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-18 17:04:30 -0300

    feat: add validator to users route

--- src/Routes/users.routes.js
+++ src/Routes/users.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const UsersController = require('../Controllers/users.controller');
+const { validateUsers } = require('../Middlewares/users.middleware');
+
 
 const route = express();
 
 route.get('/', UsersController.getAll);
 route.get('/:id', UsersController.getById);
-route.post('/', UsersController.create);
+route.post('/', validateUsers, UsersController.create);
 
 module.exports = route;
