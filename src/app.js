104b814b36ea31b009a15b96cfc878ec205e8fbe
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-18 16:07:25 -0300

    feat: add Users router to app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ require('dotenv');
 const express = require('express');
 const { errorHandler } = require('./Middlewares/error.middleware');
 const LoginRouter = require('./Routes/login.routes');
+const UsersRouter = require('./Routes/users.routes');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(errorHandler);
 
 app.use('/login', LoginRouter);
+app.use('/users', UsersRouter);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
