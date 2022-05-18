c4365c4880cc2dc7790476bedebb4534f661c44d
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-18 17:59:27 -0300

    feat: add post tasks router

--- src/Routes/tasks.routes.js
+++ src/Routes/tasks.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const TasksController = require('../Controllers/tasks.controller');
+const { validateTasks } = require('../Middlewares/tasks.middleware');
 
 const route = express();
 
 route.get('/:id', TasksController.getAll);
+route.post('/:id', validateTasks, TasksController.create);
 
 module.exports = route;
