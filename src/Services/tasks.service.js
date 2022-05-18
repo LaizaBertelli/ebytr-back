f81ecbe330dbec7268e494eaa14c6c75df039c16
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-18 18:11:03 -0300

    feat: implements edit task service

--- src/Services/tasks.service.js
+++ src/Services/tasks.service.js
@@ -32,7 +32,24 @@ const create = async (userId, status, description, title) => {
   }
 }
 
+const edit = async (id, status, description, title) => {
+  try {
+    const editedTask = await Tasks.update(
+      {
+        status,
+        description,
+        title,
+      },
+      { where: { id } });
+  
+    return editedTask;
+  } catch (e) {
+    throw new Error(e);
+  }
+}
+
 module.exports = {
   getAll,
-  create
+  create,
+  edit,
 };
