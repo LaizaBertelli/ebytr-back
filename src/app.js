8824b3b660bf38dd0bcf9b5c565e33a9fb69f973
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 17:42:58 -0300

    feat: enable cors for real

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv');
 
 const express = require('express');
+const cors = require('cors');
 const { errorHandler } = require('./Middlewares/error.middleware');
 const LoginRouter = require('./Routes/login.routes');
 const UsersRouter = require('./Routes/users.routes');
@@ -8,6 +9,7 @@ const TasksRouter = require('./Routes/tasks.routes');
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(errorHandler);
 
@@ -19,14 +21,7 @@ app.use('/login', LoginRouter);
 app.use('/users', UsersRouter);
 app.use('/tasks', TasksRouter);
 
-// Enable CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+app.listen(3040, () => {
   console.log(`App listening on port ${PORT}`);
 });
