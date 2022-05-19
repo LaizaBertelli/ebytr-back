f0323a629438ef328f1bda8b4a8ff5d183ca6f3c
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 01:14:00 -0300

    feat: create temporary root endpoint

--- src/app.js
+++ src/app.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(errorHandler);
 
+app.get('/', (req, res) => {
+  return res.status(200).json('Welcome to task manager api');
+})
+
 app.use('/login', LoginRouter);
 app.use('/users', UsersRouter);
 app.use('/tasks', TasksRouter);
