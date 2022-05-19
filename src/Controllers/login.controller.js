d22b8ef7dc020b2d05ae8b5eba699a563b5e3c51
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 03:11:07 -0300

    fix: fix status code

--- src/Controllers/login.controller.js
+++ src/Controllers/login.controller.js
@@ -5,7 +5,7 @@ const createLogin = async (req, res, next) => {
     const { email, password } = req.body;
     const token = await LoginService.postLogin(email, password);
     console.log(token);
-    if (!token) return res.status(400).json({ message: 'Invalid email or password' });
+    if (!token) return res.status(401).json({ message: 'Invalid email or password' });
   
     return res.status(200).json({ token });
   } catch (e) {
