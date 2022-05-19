1a0350ddb5e592fc3955e94159f237a6ffe41833
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 00:59:18 -0300

    fix: fix var configs use on database config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -9,17 +9,17 @@ module.exports = {
     dialect: 'mysql',
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: 'task_manager',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: 'task_manager',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
   }
 }
