42e742d31776a967216986a5f1b93ec3f042a319
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-20 16:09:05 -0300

    feat: enable all methods in cors

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const app = express();
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+  res.headers("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 app.use(express.json());
